df37729943a13a17f5ae7d09aaa8640c9cc993df
张森林 <senlinzhang006@163.com>
2026-06-09 23:33:20 +0800

    Upload auto17 as cp1_6.9_auto17

--- cp1_6.9_auto17/src/main.tsx
+++ cp1_6.9_auto17/src/main.tsx
@@ -1,18 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 )
-
-window.addEventListener('load', () => {
-  const el = document.getElementById('loading')
-  if (el) {
-    setTimeout(() => el.classList.add('hide'), 400)
-    setTimeout(() => el.remove(), 1100)
-  }
-})
