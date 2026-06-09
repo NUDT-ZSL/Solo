f0b5179beea5d378e7ac727186b1905111a72f5d
张森林 <senlinzhang006@163.com>
2026-06-09 23:33:27 +0800

    Upload auto40 as cp1_6.9_auto40

--- cp1_6.9_auto40/vite.config.js
+++ cp1_6.9_auto40/vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    hmr: true,
-    host: true
+    host: true,
+    port: 5173,
+    open: true
+  },
+  optimizeDeps: {
+    exclude: ['@react-three/fiber', '@react-three/drei']
   }
 })
