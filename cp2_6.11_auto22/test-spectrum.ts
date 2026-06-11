6370d76b74ee3150714ff75b659f9ea977cd2902
张森林 <senlinzhang006@163.com>
2026-06-11 12:55:49 +0800

    Round 1: Upload auto22 as cp2_6.11_auto22

--- cp2_6.11_auto22/test-spectrum.ts
+++
@@ -1,8 +0,0 @@
-import { analyzeSpectrum } from './api/services/spectrumAnalyzer.js';
-
-async function main() {
-  const r = await analyzeSpectrum('test-audio.wav');
-  console.log('WAV result:', JSON.stringify(r, null, 2));
-}
-
-main().catch(console.error);
