40183984d35defc227026a42912d677b8d5845e2
张森林 <senlinzhang006@163.com>
2026-06-14 10:46:01 +0800

    Round 3: Upload auto82 as cp1_6.14_auto82

---
+++ cp1_6.14_auto82/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import LevelEditor from './editor';
+
+export default function App() {
+  return (
+    <div style={{ width: '100%', height: '100%' }}>
+      <LevelEditor />
+    </div>
+  );
+}
--- cp1_6.14_auto82/src/main.tsx
+++ cp1_6.14_auto82/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import LevelEditor from './editor';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <LevelEditor />
+    <App />
   </React.StrictMode>
 );
