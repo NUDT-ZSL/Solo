659f27d51cf481a4a8dcfa749acb7f0b455d6ef7
张森林 <senlinzhang006@163.com>
2026-06-14 16:25:24 +0800

    Round 2: Upload auto134 as cp1_6.14_auto134

---
+++ cp1_6.14_auto134/src/utils/event-bus.ts
@@ -0,0 +1,29 @@
+type EventCallback = (...args: any[]) => void;
+
+class EventBus {
+  private listeners: Map<string, Set<EventCallback>> = new Map();
+
+  on(event: string, callback: EventCallback): () => void {
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, new Set());
+    }
+    this.listeners.get(event)!.add(callback);
+    return () => this.off(event, callback);
+  }
+
+  off(event: string, callback: EventCallback): void {
+    this.listeners.get(event)?.delete(callback);
+  }
+
+  emit(event: string, ...args: any[]): void {
+    this.listeners.get(event)?.forEach((cb) => cb(...args));
+  }
+
+  clear(): void {
+    this.listeners.clear();
+  }
+}
+
+export const eventBus = new EventBus();
+
+export default eventBus;
