12764f817205456a4f39e82f89a34a5bdac6c894
张森林 <senlinzhang006@163.com>
2026-06-14 14:27:39 +0800

    Round 2: Upload auto118 as cp1_6.14_auto118

--- cp1_6.14_auto118/src/main.tsx
+++ cp1_6.14_auto118/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
+import App from './client/App'
+import './client/styles/global.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
