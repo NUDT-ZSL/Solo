e2756749b5cdbffe1782d310ec21dd01e262cd4e
张森林 <senlinzhang006@163.com>
2026-06-15 14:07:13 +0800

    Round 2: Upload auto14 as cp2_6.15_auto14

--- cp2_6.15_auto14/src/App.tsx
+++ cp2_6.15_auto14/src/App.tsx
@@ -1,13 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "@/pages/Home";
+import { useState } from 'react';
+import ProjectCreate from './components/ProjectCreate';
+import PixelEditor from './PixelEditor';
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/other" element={<div className="text-center text-xl">Other Page - Coming Soon</div>} />
-      </Routes>
-    </Router>
-  );
+  const [projectSize, setProjectSize] = useState<{ w: number; h: number } | null>(null);
+
+  if (!projectSize) {
+    return <ProjectCreate onConfirm={(w, h) => setProjectSize({ w, h })} />;
+  }
+
+  return <PixelEditor width={projectSize.w} height={projectSize.h} />;
 }
